b4ecbfbe0ab0d5d45e65f935d511463deba9b058
mubashirnoori <mubashirbaltistani1@gmail.com>
2026-07-29 12:32:42 +0500

    message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.akhoonzadaholdings.school"
     minSdk = 24
     targetSdk = 36
-    versionCode = 12
-    versionName = "1.0.2"
+    versionCode = 13
+    versionName = "1.0.3"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
 
